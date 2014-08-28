d01dba865a98e890fa9c7965ad12f25686c6aa81
maxiaochao <ant_shake_tree@163.com>
2014-08-28 15:39:55 +0800

    ccccccccccc

--- vdi-core-server-lls-828/src/main/java/com/vdi/support/desktop/lls/services/TemplateService.java
+++
@@ -1,7 +0,0 @@
-package com.vdi.support.desktop.lls.services;
-
-import org.springframework.stereotype.Service;
-@Service
-public interface TemplateService {
-
-}
--- vdi-core-server-lls-828/target/classes/init.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM t_role;
-insert into t_role values(1,'ROLE_ADMIN+',0);
-insert into t_role values(2,'ROLE_USER+',0);
-
